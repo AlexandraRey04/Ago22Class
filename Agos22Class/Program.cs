dc93159ab3805abe13442c67bf210e112ad04514
Alexandra Rey E <142675848+AlexandraRey04@users.noreply.github.com>
2023-08-22 20:36:25 -0500

    Configuración inicial

---
+++ Agos22Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Agos22Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
