d0ad87c8baebee7a279dba24c385ea4c6e5cb21a
Alexandra Rey E <142675848+AlexandraRey04@users.noreply.github.com>
2023-08-26 10:28:58 -0500

    Ejercicio terminado

--- Agos22Class/Exercise1.cs
+++ Agos22Class/Exercise1.cs
@@ -4,18 +4,57 @@
     {
         public static void ExecuteExercise()
         {
+            Console.WriteLine("Conversión de centímetros a otras unidades de medida");
 
-            double p, r;
-            double pi = 3.1416;
+            Console.Write("Ingresa la longitud en centímetros: ");
+            double centimetros = Convert.ToDouble(Console.ReadLine());
 
+            double yardas = centimetros / 91.44;  // 1 yarda = 91.44 centímetros
+            double metros = centimetros / 100;     // 1 metro = 100 centímetros
+            double pies = centimetros / 30.48;     // 1 pie = 30.48 centímetros
+            double pulgadas = centimetros / 2.54;  // 1 pulgada = 2.54 centímetros
 
-            Console.WriteLine("Ingrese el radio del circulo");
+            Console.WriteLine("Longitud en yardas:" + yardas);
+            Console.WriteLine("Longitud en metros:" + metros);
+            Console.WriteLine("Longitud en pies:" + pies);
+            Console.WriteLine("Longitud en pulgadas:" + pulgadas);
+        }
 
-            double v = double.Parse(Console.ReadLine());
-            r = v;
-            p = (2 * pi) * r;
-            Console.WriteLine("El perimetro es" + p);
+        public static void ExecuteExercise2()
+        {
+            Console.WriteLine("Verificación de divisibilidad");
 
+            Console.Write("Ingresa el primer número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa el segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num2 % num1 == 0)
+            {
+                Console.WriteLine("El segundo número es divisible por el primero.");
+            }
+            else
+            {
+                Console.WriteLine("El segundo número no es divisible por el primero.");
+            }
+        }
+        public static void ExecuteExercise3()
+        {
+            Console.WriteLine("Cálculo del porcentaje de hombres y mujeres en un grupo");
+
+            Console.Write("Ingrese el total de hombres: ");
+            int totalHombres = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el total de mujeres: ");
+            int totalMujeres = Convert.ToInt32(Console.ReadLine());
+
+            int totalPersonas = totalHombres + totalMujeres;
+
+            double porcentajeHombres = (double)totalHombres / totalPersonas * 100;
+            double porcentajeMujeres = (double)totalMujeres / totalPersonas * 100;
+
+            Console.WriteLine("El porcentaje de hombres: " + porcentajeHombres);
+            Console.WriteLine("El porcentaje de mujeres: " + porcentajeMujeres);
         }
     }
 }
--- Agos22Class/Program.cs
+++ Agos22Class/Program.cs
@@ -8,24 +8,24 @@
 
             //Menu para seleccionar que programa ejecutar 
 
-            int ejercicio = 3;
+            int ejercicio = 0;
+            Console.WriteLine("Seleccione el ejercio que desea realixar\n 1. Conversión\n 2. Números divisibles\n 3. Promedio población");
+            ejercicio = int.Parse(Console.ReadLine());
             switch (ejercicio)
             {
                 case 1:
                     
                     Exercise1.ExecuteExercise();
-
+                    Console.WriteLine("Ejercicio conversión");
                     break;
                 case 2:
-                    Console.WriteLine("Exersice2");
+                    Exercise1.ExecuteExercise2();
+                    Console.WriteLine("Ejercicio números divisibles");
                     break;
                 case 3:
-                    Console.WriteLine("Exersice3");
-
+                    Exercise1.ExecuteExercise3();
+                    Console.WriteLine("Promedio población");
                     break;
-
-
-
             }
         }
 
